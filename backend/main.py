076f989ef6f03126ca529affa76e1203fc92ab55
kayne-lee <kayne.lee2@outlook.com>
2025-06-21 01:05:23 -0400

    setup supabase

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from supabase.client import create_client, Client 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello Hackathon 👋"}
+    return {"message": "Supabase connected!"}
+
+@app.get("/users")
+def get_users():
+    return supabase.table("users").select("*").execute().data
