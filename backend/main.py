441ec27a7d4975c7c9caaccf8e193d4fe4a0eefa
ChloeHouvardas <chloehouvardas404@gmail.com>
2025-06-21 11:25:28 -0400

    test endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+# env vars
+load_dotenv()
+
+SUPABASE_URL=os.getenv("SUPABASE_URL")
+SUPABASE_KEY=os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
+
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
 
 @app.get("/")
 def read_root():
     return {"message": "Hello Hackathon 👋"}
+
+@app.post("/users")
+def add_user(user: UserCreate):
+    response = supabase.table("users").insert({
+        "fname": user.first_name,
+        "lname": user.last_name
+    }).execute()
+
+    if not response.data:  # Insert failed, no data returned
+        return {"error": "Failed to insert user."}
+    
+    return {"message": "User added successfully", "data": response.data}
+
+
---
+++ backend/scripts/tag_verification.py
@@ -0,0 +1,3 @@
+'''
+Given tags of an image, check the db and compare with existing tags. If image tag doesnt match any current tag in the db then add it
+'''
