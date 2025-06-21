efe6dffddaaea16e98c0e04abb4ef6955c943cc0
kayne-lee <kayne.lee2@outlook.com>
2025-06-21 09:51:20 -0400

    first endpoint complete

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,23 @@ supabase: Client = create_client(url, key)
 
 app = FastAPI()
 
+SUPABASE_BUCKET_URL = "https://ftwuonnxcfpinajqnacp.supabase.co/storage/v1/object/public/listings//"
+
 @app.get("/")
 def read_root():
-    return {"message": "Supabase connected!"}
+    listings = supabase.table("listings").select("*").execute().data
+    result = []
+    for row in listings:
+        result.append({
+            "id": row["id"],
+            "title": row["title"],
+            "description": row["description"],
+            "price": row["price"],
+            "location": row["location"],
+            "tags": row["tags"],
+            "image_url": f"{SUPABASE_BUCKET_URL}{row['picture']}"
+        })
+    return result
 
 @app.get("/users")
 def get_users():
